dfc4fc2b34f946ed02fa9d2198859e2ce7ebf509
sindhurajesh123 <usindhu1988@gmail.com>
2023-11-07 11:36:17 +0530

    final

---
+++ weather.js
@@ -0,0 +1,37 @@
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=imperial
+
+let API_KEY = "7a7343a473b7f1a0ce0c4896f2e7a58b";
+  getWeatherData = (city) =>{
+  const URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+  const Full_Url = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise = fetch(Full_Url);
+  console.log(city)
+
+  return weatherPromise.then((response) => {
+  return response.json()
+  })
+
+  
+}
+
+function searchCity(){
+    const city = document.getElementById('city-input').value;
+
+    getWeatherData(city)
+    .then((response) =>{
+      showWeatherData(response)
+   console.log(response)
+    })
+    .catch((err) =>{
+        console.log(err)
+    })
+}
+
+showWeatherData = (weatherData) =>{
+  document.getElementById('city-name').innerText = weatherData.name;
+  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+  document.getElementById('temp').innerText = weatherData.main.temp;
+  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
+}
